54d44dbbccd48fea286735aafa23a9ba6ec616e5
admin <bjl0615@gmail.com>
2023-07-19 22:50:57 +0900

    Binding

--- app/src/main/java/com/example/conversionofunits/MainActivity.kt
+++ app/src/main/java/com/example/conversionofunits/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.conversionofunits
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.conversionofunits.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val outputTextView = binding.outputTextView
+        val unitTextView = binding.outputUnitTextView
+        val inputEditText = binding.inputEditText
+        val inputUnitTextView = binding.inputUnitTextView
+
+
     }
 }
